d7c81431f0019772a5fa52561375214f60c372e6
airgirl2305 <kheda.askhabova@gmail.com>
2024-04-27 13:24:38 +0300

    editing the searching bot 1.3.1 myMegaSearchingBot.js update

--- searchingBots/searching bot 1.3.1 myMegaSearchingBot.js
+++ searchingBots/searching bot 1.3.1 myMegaSearchingBot.js
@@ -4,113 +4,124 @@
 // @version      1.1.2
 // @description  try to take over the world!
 // @author       Askhabova Kheda
-// @match        https://swisscows.com/*
 // @match        https://ya.ru/*
+// @match        https://www.bing.com/*
+// @match        https://swisscows.com/*
 // @match        https://www.nigma.net.ru/*
 // @match        https://www.google.com/*
 // @match        https://www.phind.com/*
 // @match        https://you.com/*
+// @match        https://smashingmagazine.com/*
+// @match        https://www.freecodecamp.org/*
+// @match        https://anywhere.epam.com/*
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  "use strict";
-
-  // Your code here...
-})();
-
+// (function () {
+//   "use strict";
+//
+//   // Your code here...
+// })();
+let searchEngine;
 let input;
 let button;
+let nextPageSelector;
+let currentPageSelector;
+let sites = {
+  "smashingmagazine.com": [
+    "Writing CSS In 2023",
+    "How To Search For A Developer Job Abroad",
+    "Learning JavaScript With Imagination",
+  ],
+  "anywhere.epam.com": [
+    "web developer resume", // in first results
+    "best practices for nodejs",
+    "JavaScript Portfolio",
+  ],
+  "freecodecamp.org": [
+    "How to Deploy Web", // first result
+    "Portfolio Website", // further
+    "LocalStorage",
+  ],
+};
+let sitesKeys = Object.keys(sites);
+let site = sitesKeys[getRandom(0, sitesKeys.length)];
+let keywords = sites[site];
+let keyword = keywords[getRandom(0, keywords.length)];
 let links = document.links;
-let keywords = [
-  "Writing CSS In 2023",
-  "How To Search For A Developer Job Abroad",
-  "Learning JavaScript With Imagination",
-];
+let mouseClick = new MouseEvent("click"); //?
 
-let keyword = keywords[getRandom(keywords.length, 0)];
-
-
-if ((location.href = "https://nigma")) {
-  input = document.getElementById("query");
-  button = document.querySelector('[value="Найти!"]');
-}
-if ((location.href = "https://www.bing.com/")) {
+if (location.href == "https://www.bing.com/") {
+  searchEngine = "https://www.bing.com/";
   input = document.getElementsByName("q")[0];
   button = document.getElementsByClassName("search")[0];
-}
-if ((location.href = "https://swisscows.com/")) {
+  nextPageSelector = ".sb_pagN";
+  currentPageSelector = ".sb_pagS";
+} else if (location.href == "https://www.ya.ru/") {
+  searchEngine = "https://ya.ru";
+  input = document.getElementById("text");
+  button = document.getElementsByClassName("search3__button")[0]; // hidden button for search
+  nextPageSelector = ".Pager-Item_type_next";
+  currentPageSelector = ".Pager-Item_current";
+} else if (location.href == "https://www.google.com/") {
+  searchEngine = "https://www.google.com/";
+  input = document.getElementById("input");
+  button = document.querySelector('[aria-label="Поиск в Google"]');
+  nextPageSelector = "";
+  currentPageSelector = "";
+} else if (location.href == "https://nigma.net.ru/") {
+  searchEngine = "https://nigma.net.ru/";
+  input = document.getElementById("query");
+  button = document.querySelector('[value="Найти!"]');
+  currentPageSelector = ".gsc-cursor-current-page";
+  nextPageSelector = "";
+} else if (location.href == "https://swisscows.com/") {
+  searchEngine = "https://swisscows.com/";
   input = document.querySelector('[type="search"]');
   button = document.getElementsByClassName("search-submit")[0];
-}
-if ((location.href = "https://www.phind.com/")) {
+  nextPageSelector = "";
+  currentPageSelector = "";
+} else if (location.href == "https://www.phind.com/") {
+  searchEngine = "https://www.phind.com/";
   input = document.querySelector('[placeholder="Ask anything. What are you stuck on?"]');
   button = document.getElementsByClassName("fe-arrow-right")[0];
-}
-
-if ((location.href = "https://www.you.com/")) {
+  nextPageSelector = "";
+  currentPageSelector = "";
+} else if (location.href == "https://you.com/") {
+  searchEngine = "https://www.you.com/";
   input = document.getElementById("search-input-textarea");
   button = document.querySelector('[data-eventactionname="click_send"]');
+  nextPageSelector = "";
+  currentPageSelector = "";
 }
-if ((location.href = "https://www.ya.ru/")) {
-  //input =;
-  //button = ;
-}
-
-if (cowsBtn !== undefined) {
-  cowsInput.value = keyword;
-  cowsBtn.click();
-  if (cowsBtn.click()) console.log("клик");
-} else {
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("smashingmagazine.com") != -1) {
-      let link = links[i];
-      console.log("Нашёл строку " + link);
-      link.click();
-      break;
-    }
-  }
-}
-let links = document.links;
-let site = "smashingmagazine.com";
-let searchEngine = "https://ya.ru/"
-let keywords = [
-  "CSS 2023", // page 2 - "Writing CSS In 2023",
-  "developer job abroad", // page 2 - "How To Search For A Developer Job Abroad",
-  "Learning JavaScript With Imagination",
-  "Making Sense Of “Senseless” JavaScript Features",
-];
-let keyword = keywords[getRandom(0, keywords.length)];
-
-
-//MouseEvent("click");// new user event - our bot ears
-
-//MouseEvent("click");// new user event - our bot ears
 
 // working on the search engine main page
-if (button !== undefined) { // search button is only in the main page
-  let i = 0;
-  //  input.focus();
-  //  input.dispatchEvent(mouseClick);
-  //  input.value = keyword;
-  //  button.click();
+if (button !== undefined) {
+  // search button is only in the main page
 
-  let timerId = setInterval(() => {
-    input.value += keyword[i];
-    i++;
-    if (i == keyword.length) {
-      clearInterval(timerId);
-      button.click();
-    }
-  }, 150);
+  input.focus();
+  input.dispatchEvent(mouseClick);
+  input.value = keyword;
+  button.click();
+  // let i = 0;
+  // let timerId = setInterval(() => {
+  //   input.value += keyword[i];
+  //   i++;
+  //   if (i == keyword.length) {
+  //     clearInterval(timerId);
+  //     button.click();
+  //   }
+  // }, 150);
 
+  document.cookie = `site=${site}`;
+  console.log("we are on the main page, cookie", cookie);
   // working on the target page
 } else if (location.hostname == site) {
   setInterval(() => {
     let linkIndex = getRandom(0, links.length);
     let localLink = links[linkIndex];
 
-    if (getRandom(0, 101) > 50) {
+    if (getRandom(0, 101) > 70) {
       location.href = searchEngine;
     }
     if (links.length == 0) {
@@ -121,10 +132,16 @@ if (button !== undefined) { // search button is only in the main page
     }
   }, getRandom(5000, 7000));
   console.log("We are on the target page");
+  site = getCookie("site");
 }
 
 //working on a search results page
-else if (document.querySelector(".Pager-Content") !== null) { // search results page has pages in it
+else if (document.querySelector(currentPageSelector) !== null) {
+  // instead of pages container
+  // search results page has pages in it
+
+  site = location.hostname;
+
   let nextPage = true;
   for (let i = 0; i < links.length; i++) {
     if (links[i].href.indexOf(site) !== -1) {
@@ -132,14 +149,14 @@ else if (document.querySelector(".Pager-Content") !== null) { // search results 
       let nextPage = false;
       console.log("I've found one " + link);
       setTimeout(() => {
-        link.target = "_self";
+        link.target = "_self"; // diff for ya
         link.click();
       }, getRandom(2000, 3000));
       break;
     }
   }
   let pagesExist = setInterval(() => {
-    let page = document.querySelectorAll(".Pager-Item_current")[0];
+    let page = document.querySelector(currentPageSelector);
     if (page !== null) {
       if (page.innerText == "5") {
         let nextPage = false;
@@ -151,11 +168,18 @@ else if (document.querySelector(".Pager-Content") !== null) { // search results 
 
   if (nextPage) {
     setTimeout(() => {
-      document.getElementsByClassName("Pager-Item_type_next")[0].click();
+      document.querySelector(currentPageSelector).click();
     }, getRandom(2500, 3500));
   }
 }
 
 function getRandom(max, min) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(
+    new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)")
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
