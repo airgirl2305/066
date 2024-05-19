66b42d7b022663c988759178acf3582f273afc92
airgirl2305 <kheda.askhabova@gmail.com>
2024-05-20 01:22:30 +0300

    Задание 19. Технология AJAX

--- tasks/Genealogy.php
+++ tasks/Genealogy.php
@@ -1,44 +1,83 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 
 class Person
 {
-  private $name;
-  private $lastname;
-  private $mother;
-  private $father;
+    private $name;
+    private $lastname;
+    private $mother;
+    private $father;
 
-  function __construct($name, $lastname, $mother = null, $father = null)
-  {
-    $this->name = $name;
-    $this->lastname = $lastname;
-    $this->mother = $mother;
-    $this->father = $father;
-  }
+    function __construct($name, $lastname, $mother = null, $father = null)
+    {
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->mother = $mother;
+        $this->father = $father;
+    }
 
-  function getName()
-  {
-    return $this->name;
-  }
-  function getMother()
-  {
-    return $this->mother;
-  }
-  function getFather()
-  {
-    return $this->father;
-  }
+    function getInfo()
+    {
+        $motherName = $this->getMother() ? $this->getMother()->getName() : "";
+        $fatherName = $this->getFather() ? $this->getFather()->getName() : "";
+        $maternalGrandMother = $this->getMother()->getMother() ? $this->getMother()->getMother()->getName() : "";
+        $maternalGrandFather = $this->getMother()->getFather() ? $this->getMother()->getFather()->getName() : "";
+        $paternalGrandMother = $this->getFather()->getMother() ? $this->getFather()->getMother()->getName() : "";
+        $paternalGrandFather = $this->getFather()->getFather() ? $this->getFather()->getFather()->getName() : "";
+        if ($motherName && $fatherName) {
+            $parents = ", моих родителей " . $motherName . " и " . $fatherName;
+        } elseif ($motherName) {
+            $parents = ", мою маму " . $motherName;
+        } elseif ($fatherName) {
+            $parents = ", моего отца " . $fatherName;
+        } else {
+            $parents = "";
+        }
 
-  function getInfo()
-  {
-    return "Меня зовут - " . $this->getName() . "<br> Моих родителей зовут " . $this->getMother()->getName() . $this->getFather()->getName() . "<br> Моих бабушку и дедушку по линии матери зовут " . $this->getMother()->getMother()->getName() . $this->getMother()->getFather()->getName() . "<br> Моих бабушку и дедушку по линии отца зовут " . $this->getFather()->getMother()->getName() . $this->getFather()->getFather()->getName();
-  }
+        if ($maternalGrandMother && $maternalGrandFather) {
+            $maternal = "Мои бабушка и дедушка по маме: " . $maternalGrandMother . " и " . $maternalGrandFather . ". <br> ";
+        } elseif ($maternalGrandMother) {
+            $maternal = "Бабушку по маме - " . $maternalGrandMother . ". <br> ";
+        } elseif ($maternalGrandFather) {
+            $maternal = "Дедушка по маме - " . $maternalGrandFather . ". <br> ";
+        } else {
+            $maternal = "";
+        }
+
+        if ($paternalGrandMother && $paternalGrandFather) {
+            $paternal = "Cтаршие по линии отца: " . $paternalGrandMother . " и " . $paternalGrandFather . ".";
+        } elseif ($paternalGrandMother) {
+            $paternal = "Бабушку по линии отца зовут " . $paternalGrandMother . ".";
+        } elseif ($paternalGrandFather) {
+            $paternal = "Дедушку по линии отца зовут " . $paternalGrandFather . ".";
+        } else {
+            $paternal = "";
+        }
+        return "Меня зовут " . $this->getName() . $parents . ". <br> " . $maternal . $paternal;
+    }
+
+    function getMother()
+    {
+        return $this->mother;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function getFather()
+    {
+        return $this->father;
+    }
 }
 
-$pavel =
-  new Person("Павел", "Петров");
-$olga = new Person("Ольга", "Иванова", null, $pavel);
+$pavel = new Person("Павел", "Петров");
+$ekaterina = new Person("Екатарина", "Петрова");
 $anna = new Person("Анна", "Иванова");
 $igor = new Person("Игорь", "Иванов");
+$olga = new Person("Ольга", "Иванова", null, $pavel);
+// $olga = new Person("Ольга", "Иванова", $ekaterina, $pavel);
 $alex = new Person("Александр", "Иванов", $anna, $igor);
 $valera = new Person("Валерий", "Иванов", $olga, $alex);
 
