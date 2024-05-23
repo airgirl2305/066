1c7243e6ed378ac4d10298c0b7d094e4fdf6081b
airgirl2305 <kheda.askhabova@gmail.com>
2024-05-23 16:31:34 +0300

    rearrangement

--- tasks/Aroma Shop/php/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-
-$host = "localhost";
-$db = "l96705y2_066";
-$user = "l96705y2_066";
-$password = "fR4R*4Cn";
-
-$mysqli = mysqli_connect($host, $user, $password, $db);
